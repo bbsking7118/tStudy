f7833ded8a864a061398edb55d8a5074fc125d2a
bbsking7118 <bbsking1@naver.com>
2023-01-22 03:44:10 +0900

    cors

--- back/config/express.js
+++ back/config/express.js
@@ -11,7 +11,11 @@ module.exports = function () {
   app.use(express.json()); // body값을 파싱
   app.use(express.urlencoded({ extended: true })); // form 으로 제출되는 값 파싱
   app.use(methodOverride()); // put, delete 요청 처리
-  app.use(cors()); // 웹브라우저 cors 설정을 관리
+  app.use(cors({
+    origin: "https://www.east2000.co.kr", // 접근 권한을 부여하는 도메인
+    credentials: true, // 응답 헤더에 Access-Control-Allow-Credentials 추가
+    optionsSuccessStatus: 200, // 응답 상태 200으로 설정
+  })); // 웹브라우저 cors 설정을 관리
   app.use(express.static("/home/ubuntu/tStudy/front")); // express 정적 파일 제공 (html, css, js 등..)
   // app.use(express.static(process.cwd() + '/public'));
 
