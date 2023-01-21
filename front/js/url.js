d432788f53cf73ab72b5b2b6efd69e86d73834d1
bbsking7118 <bbsking1@naver.com>
2023-01-22 02:08:50 +0900

    https인증

--- front/js/url.js
+++ front/js/url.js
@@ -1,3 +1,4 @@
 // 서버에 API를 요청할 때 사용할 공통적인 url을 정의합니다.
 // const url = "https://www.seongong.shop";
-const url = "http://43.201.145.183:3001";
+//const url = "http://43.201.145.183:3001";
+const url = "https://www.east2000.co.kr:3001";
